4c27cdbeb8520501ef1e0045a1a8a10665b84767
Jake Webster <jakewebs2@gmail.com>
2021-11-28 23:15:02 -0800

    Used Node.js and the Requests API to get the tickets using CL inputs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-About this repo:
 
+**COMPILATION**
+To run the code, go to the terminal in the zendesk folder and enter "node script.js [EMAIL] [TOKEN]", where email and token correspond to your inputs for those for the program. 
+
+In order to properly pull the tokens, one set of working credentials is (EMAIL = "jakewebster2023@berkeley.edu", TOKEN = "7bpia9Ypln87GZOAXgnGRIKLQrD7p8rpy6aQONTL").
--- process.py
+++ process.py
@@ -1,27 +1,35 @@
 import requests
-
+import sys
+import json
 '''
 	This file serves two functions.
 
 	1. Connect to the Zendesk API and request all tickets for my account, 
 	   which is at the domain zccjakewebs.zendesk.com.
-	2. Parse the returned string to extract needed information for the rest
-	   of the project.
+	2. Print out the tickets if successfully found, or an error message if not.
 '''
 
 # Parameters
-url = 'https://zccjakewebs.zendesk.com/api/v2/groups.json'
-user = 'jakewebster2023@berkeley.edu' + '/token'
-password = '7bpia9Ypln87GZOAXgnGRIKLQrD7p8rpy6aQONTL' # Token generated using Zendesk API
+url = 'https://zccjakewebs.zendesk.com/api/v2/tickets.json'
+#Correct set of parameters for reference purposes
+# EMAIL = 'jakewebster2023@berkeley.edu' + '/token'
+# PASSWORD = '7bpia9Ypln87GZOAXgnGRIKLQrD7p8rpy6aQONTL' (Token generated using Zendesk API)
+user = sys.argv[1] + '/token'
+password = sys.argv[2]
+print("Inputted username: ", user)
+print("Inputted access token: ", password)
 
 response = requests.get(url, auth = (user, password))
 
 # Check for bad HTTP code
 sc = response.status_code
 if sc != 200:
-	print('Status:', sc, 'Request failed. Exiting.')
-	exit()
-
-# Basic print statement for now
-data = response.json()
-print(data)
+	print("Login failed! Exiting the program.")
+	#exit()
+else:
+	# Print out the tickets
+	data = response.json()
+	tickets = data['tickets']
+	print("Login successful! ")
+	print("---- TICKETS ----")
+	print(tickets)
